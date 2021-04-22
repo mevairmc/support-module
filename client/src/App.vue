a30aa6387336b544c148d6348d859a7ac53967ab
Marco Evair <mmacolluncoc@uni.pe>
2021-04-21 22:11:28 -0500

    add vue router and style app page

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,31 +1,68 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    Hello
-    <newRequest/>
+    <nav class="nav">
+      <img src="./assets/planner1.jpeg" alt="logo">
+      <router-link to="/new">
+        <v-icon name="paper-plane" scale="1" />
+          New
+      </router-link>
+      <router-link to="/list">
+        <v-icon name="inbox" scale="1" />
+          List
+      </router-link>
+    </nav>
+    <router-view/>
   </div>
 </template>
 
 <script>
-
-import newRequest from './layouts/newRequest'
+import 'vue-awesome/icons'
+import Icon from 'vue-awesome/components/Icon'
 
 export default {
-  name: 'App',
   components: {
-
-    newRequest
+    'v-icon': Icon
   }
 }
 </script>
 
 <style>
+body {
+  margin: 0
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.nav {
+  display: flex;
+  flex-direction: column;
+  width: 250px;
+  height: 100vh;
+  background-color: #F7F7F7;
+}
+
+.nav a {
+  color: #2c3e50;
+  text-decoration-line: none;
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  height: 50px;
+  padding-left: 25px;
+}
+
+.nav a:hover {
+  background-color: rgba(47, 114, 226, 0.8);
+  color: #f7f7f7
+}
+
+.nav a.router-link-exact-active {
+  color: #f7f7f7;
+  background-color: rgb(34, 29, 129);
 }
 </style>
---
+++ client/src/views/New.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <newRequest/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import newRequest from '@/layouts/newRequest'
+
+export default {
+  name: 'new',
+  components: {
+    newRequest
+  }
+}
+</script>
